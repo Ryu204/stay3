85068c65f7c9734f006a39bcea865607356e33e3
Ryu204 <nguyenanhbao2356@gmail.com>
2025-01-31 20:36:45 +0700

    chores: catch2

--- test/core/time.test.cpp
+++ test/core/time.test.cpp
@@ -1,6 +1,6 @@
-#include <cassert>
 #include <chrono>
 #include <thread>
+#include <catch2/catch_test_macros.hpp>
 
 import stay3;
 
@@ -10,41 +10,41 @@ constexpr auto sleep_ms = 100;
 constexpr auto acceptable_margin_ms = 50;
 constexpr auto ms_per_s = 1000.F;
 
-void test_stop_watch_time_since_start() {
+TEST_CASE("stop_watch time_since_start") {
     stop_watch watch;
 
-    const seconds first_timestamp = watch.time_since_start();
-    assert(first_timestamp >= 0.F && first_timestamp <= 1.F && "Initial time_since_start should not be in [0, 1]");
+    SECTION("Initial timestamp should be within [0,1] seconds") {
+        const seconds first_timestamp = watch.time_since_start();
+        REQUIRE(first_timestamp >= 0.F);
+        REQUIRE(first_timestamp <= 1.F);
+    }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
-    const seconds time_after_sleep = watch.time_since_start();
-    assert(time_after_sleep > first_timestamp && "Time should increase after sleep");
-
-    assert(
-        time_after_sleep >= sleep_ms / ms_per_s
-        && time_after_sleep <= (sleep_ms + acceptable_margin_ms) / ms_per_s
-        && "Time should be within expected range after sleep");
+    SECTION("Time progresses after sleep") {
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
+        const seconds time_after_sleep = watch.time_since_start();
+        REQUIRE(time_after_sleep > 0.F);
+        REQUIRE(time_after_sleep >= sleep_ms / ms_per_s);
+        REQUIRE(time_after_sleep <= (sleep_ms + acceptable_margin_ms) / ms_per_s);
+    }
 }
 
-void test_stop_watch_restart() {
+TEST_CASE("stop_watch restart behavior") {
     stop_watch watch;
 
-    const auto time_before_restart = watch.restart();
-    const auto time_after_restart = watch.restart();
+    SECTION("Restart should reset time close to zero") {
+        const auto time_before_restart = watch.restart();
+        REQUIRE(time_before_restart >= 0.F);
+        REQUIRE(time_before_restart <= 1.F);
 
-    assert(time_after_restart >= 0.F && time_after_restart <= 1.F && "Time after restart should be in [0, 1]");
+        const auto time_after_restart = watch.restart();
+        REQUIRE(time_after_restart >= 0.F);
+        REQUIRE(time_after_restart <= 1.F);
+    }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
-    const auto time_after_second_sleep = watch.restart();
-    assert(
-        time_after_second_sleep >= sleep_ms / ms_per_s
-        && time_after_second_sleep <= (sleep_ms + acceptable_margin_ms) / ms_per_s
-        && "Time after sleep should be a bit more than sleep time");
-}
-
-int main() {
-    test_stop_watch_time_since_start();
-    test_stop_watch_restart();
-
-    return 0;
+    SECTION("Restart after sleep should return elapsed time") {
+        std::this_thread::sleep_for(std::chrono::milliseconds(sleep_ms));
+        const auto time_after_sleep = watch.restart();
+        REQUIRE(time_after_sleep >= sleep_ms / ms_per_s);
+        REQUIRE(time_after_sleep <= (sleep_ms + acceptable_margin_ms) / ms_per_s);
+    }
 }
