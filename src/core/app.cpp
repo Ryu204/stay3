f0bd16089d259ad708b1cf0c71087adadd0366a8
Ryu204 <nguyenanhbao2356@gmail.com>
2025-02-01 16:28:20 +0700

    feat: emscripten build

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -1,7 +1,9 @@
 module;
 
 #include <iostream>
-
+#ifdef __EMSCRIPTEN__
+#    include <emscripten.h>
+#endif
 module stay3.core;
 
 import :time;
@@ -11,18 +13,28 @@ constexpr auto temp_fps = 60.F;
 namespace st {
 
 void app::run() {
-    stop_watch app_stop_watch;
-    auto elapsed_time = app_stop_watch.restart();
-    constexpr seconds time_per_update = 1.F / temp_fps;
+#ifdef __EMSCRIPTEN__
+    constexpr auto func = [](void *p_app) {
+        static_cast<app *>(p_app)->on_frame();
+    };
+    emscripten_set_main_loop_arg(func, static_cast<void *>(this), 0, true);
+#else
     while(true) {
-        elapsed_time += app_stop_watch.restart();
-        while(elapsed_time > time_per_update) {
-            elapsed_time -= time_per_update;
-            input();
-            update(time_per_update);
-        }
-        render();
+        on_frame();
     }
+#endif
+}
+
+void app::on_frame() {
+    const auto elapsed_time = m_watch.restart();
+    m_pending_time += elapsed_time;
+    constexpr seconds time_per_update = 1.F / temp_fps;
+    while(m_pending_time > time_per_update) {
+        m_pending_time -= time_per_update;
+        input();
+        update(time_per_update);
+    }
+    render();
 }
 
 void app::update(seconds dt) {
