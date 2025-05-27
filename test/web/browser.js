bc466fd7a9fcb192e3a94ee92b0b1d71d7fa7cd2
Ryu204 <nguyenanhbao2356@gmail.com>
2025-05-27 10:49:13 +0700

    imma use stock chromium

--- test/web/browser.js
+++ test/web/browser.js
@@ -3,6 +3,7 @@ import { launch, defaultArgs } from "puppeteer";
 console.log(defaultArgs());
 const browser = await launch({
     headless: false,
+    executablePath: process.env.CHROME_PATH,
     args: [
         "--no-sandbox",
         "--enable-unsafe-webgpu",
